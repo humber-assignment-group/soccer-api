7b9f71435084cf7b2a7433229fb0b0623110130b
Sandeep626 <singh.sandeep.0094@gmail.com>
2021-04-28 20:35:24 -0400

    LeagueResource

--- src/main/java/org/github/humbergroup/api/LeagueResource.java
+++ src/main/java/org/github/humbergroup/api/LeagueResource.java
@@ -1,4 +1,33 @@
 package org.github.humbergroup.api;
 
+import com.github.humbergroup.soccer.LeagueService;
+import com.github.humbergroup.soccer.model.League;
+
+
+import javax.ejb.EJB;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/leagues")
 public class LeagueResource {
+
+    @EJB
+    private LeagueService leagueService;
+
+    @GET
+    @Path("/ping")
+    public Response ping(){
+        return Response.ok().entity("Service is working").build();
+    }
+
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response createLeauge(League league){
+        leagueService.create(league);
+        return Response.status(Response.Status.CREATED).entity(league).build();
+    }
+
+
 }
