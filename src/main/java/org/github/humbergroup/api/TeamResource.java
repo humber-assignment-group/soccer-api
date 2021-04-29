9cf3ecb6e8d24151feaed65c49173f402a0fa970
Sandeep626 <singh.sandeep.0094@gmail.com>
2021-04-28 20:53:11 -0400

    TeamResource

--- src/main/java/org/github/humbergroup/api/TeamResource.java
+++ src/main/java/org/github/humbergroup/api/TeamResource.java
@@ -1,5 +1,65 @@
 package org.github.humbergroup.api;
 
+import com.github.humbergroup.soccer.TeamService;
+import com.github.humbergroup.soccer.model.Team;
+import javax.ejb.EJB;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/teams")
 public class TeamResource {
 
+    @EJB
+    private TeamService teamService;
+
+    @GET
+    @Path("/ping")
+    public Response ping(){
+        return Response.ok().entity("Service is working").build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getTeamsById(long id){
+        return Response.ok().entity(teamService.getTeamById(id)).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getTeamsByLeagueId(long id){
+        return Response.ok().entity(teamService.getTeamsByLeagueId(id)).build();
+    }
+
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response createTeam(Team team){
+        teamService.createTeam(team);
+        return Response.status(Response.Status.CREATED).entity(team).build();
+    }
+
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response createTeamWithLeague(Team team, Long leagueId){
+        teamService.createTeamWithLeagueId(team, leagueId);
+        return Response.status(Response.Status.CREATED).entity(team).build();
+    }
+
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response addTeamIntoLeauge(Long teamId, Long leagueId){
+        teamService.addTeamIntoLeague(teamId, leagueId);
+        return Response.status(Response.Status.CREATED).entity("Added").build();
+    }
+
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response updateTeamName(Long teamId, String name){
+        teamService.updateTeamName(teamId, name);
+        return Response.status(Response.Status.CREATED).entity("Update").build();
+    }
 }
